b75b1e27559c7f9bbd8b61547601d6c4d87eb845
Piotr <piotrekert90@gmail.com>
2020-04-13 08:23:18 +0200

    Working on input_page layout - splitting view into three parts.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,41 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI Calculator'),
       ),
-      body: Center(
-        child: Text('Hello World'),
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  height: 200.0,
+                  width: 170.0,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  height: 200.0,
+                  width: 170.0,
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Text(''),
+          ),
+          Expanded(
+            child: Text(''),
+          ),
+        ],
       ),
     );
   }
