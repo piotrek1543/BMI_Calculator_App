a93e0a3aa242cdceef7f2abe7165dfcc5277225d
Piotr <piotrekert90@gmail.com>
2020-04-13 09:10:44 +0200

    Add icon_content.dart and font_awesome_flutter dependency.

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,5 +1,7 @@
+import 'package:bmicalculator/components/icon_content.dart';
 import 'package:bmicalculator/components/reusable_card.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -22,12 +24,20 @@ class _InputPageState extends State<InputPage> {
                   flex: 1,
                   child: ReusableCard(
                     color: Color(0xFF1D1E33),
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
+                    ),
                   ),
                 ),
                 Expanded(
                   flex: 1,
                   child: ReusableCard(
                     color: Color(0xFF1D1E33),
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: 'FEMALE',
+                    ),
                   ),
                 ),
               ],
